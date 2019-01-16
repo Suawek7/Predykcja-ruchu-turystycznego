b1d8aad8221a549c5426b9b386c4b14481609d93
Suawek7 <slawniedd@gmail.com>
2019-01-16 19:14:32 +0100

    Nowa predykcja 2

--- App/views.py
+++ App/views.py
@@ -1,73 +1,63 @@
-from django.shortcuts import render
-from django.conf import settings
-from matplotlib import pyplot as plt
-import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.datasets.samples_generator import make_blobs
 import numpy as np
 import random
-import pandas as pd
-import math
-import preprocessing
-from sklearn.datasets import load_iris
-from sklearn.neural_network import MLPClassifier
-from sklearn.linear_model import Perceptron
 from django.shortcuts import render
-from django.contrib import messages
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import svm
-from sklearn import datasets
-# Create your views here.
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.template import loader
 from .models import TStat14
 from .models import TStat15
 from .models import TStat16
 from .models import TStat17
-from django.urls import reverse
-import numpy
-from django.core import serializers
 from django.shortcuts import redirect
 from .forms import SelectProvince_Months_Form
 
 
+def index(request):
+
+    if request.method == "POST":  # jeżeli formularz został wypełniony
+        form = SelectProvince_Months_Form(request.POST)
+        if form.is_valid():  # jeżeli formularz jest poprawny
+            #  zapisz dane z formularza do sesji
+            request.session['provinces'] = request.POST['provinces']
+            request.session['month_from'] = request.POST['month_from']
+            request.session['month_to'] = request.POST['month_to']
+            return redirect('result') # przejdź do widoku result
+    else:  # jeżeli formularz nie został wypełniony
+        form = SelectProvince_Months_Form()   # utwórz nową instancję formularza
+    return render(request, 'App/index.html', {'form': form}) # wyrenderuj stronę z wzorca index.html
 
 
 def sigmoid(x):
+    # sigmoid to funkcja aktywująca, którą wykorzystywana jest w sieciach
+    # neuronowych, i z jej pomocą wyznaczamy neurony sigmoidalne
     return 1/(1+np.exp(-x))
 
 
-def sigmoid_p(x):
+def sigmoid_p(x):  # sigmoid prime - pochodna funkcji sigmoid
     return sigmoid(x) * (1-sigmoid(x))
 
 
 def train(data):
-    # random init of weights
+    # zainicjowanie wag dla wartości pierwszej, drugiej, trzeciej, oraz
     w1 = random.uniform(0.25, 0.35)
     w2 = random.uniform(0.35, 0.45)
     w3 = random.uniform(0.45, 0.55)
     b = random.uniform(0.35, 0.45)
 
-    iterations = 10000
+    iterations = 10000  # ilość iteracji
     learning_rate = 0.1
     costs = []  # keep costs during training, see if they go down
 
-    for i in range(iterations):
+    for i in range(iterations):  # wchodzimy do pętli tyle razy, ile ustawiliśmy iteracjii
         # get a random point
-        ri = np.random.randint(len(data))
+        ri = np.random.randint(len(data))  # losowy index od 0 do ilości elementów w tablicy data
         point = data[ri]
 
-        z = point[0] * w1 + point[1] * w2 + point[2] * w3 + b
+        z = point[0] * w1 + point[1] * w2 + point[2] * w3 + b  # wartość predykcji na podstawie wag wraz z
+        # danymi dla poszczególnych lat i parametru b - "bias"
         pred = sigmoid(z)  # networks prediction
 
-        target = point[3]
+        target = point[3]  # czwarta wartość statystyczna
 
-        # cost for current random point
-        cost = np.square(pred - target)
+        # koszt aktualnej wartości predykcji
+        cost = np.square(pred - target)  # np.square to funkcja potęgująca
 
         # print the cost over all data points every 1k iters
         if i % 100 == 0:
@@ -78,13 +68,14 @@ def train(data):
                 c += np.square(p_pred - p[3])
             costs.append(c)
 
-        dcost_dpred = 2 * (pred - target)
-        dpred_dz = sigmoid_p(z)
+        dcost_dpred = 2 * (pred - target)  # pochodna z kosztu predykcji
+        dpred_dz = sigmoid_p(z)  # pochodna z wartości predykcji
 
-        dz_dw1 = point[0]
-        dz_dw2 = point[1]
-        dz_dw3 = point[2]
-        dz_db = 1
+        dz_dw1 = point[0]  # pierwsza losowa wartość z wysolowanej wcześniej tabeli
+        dz_dw2 = point[1]  # druga losowa wartość z wysolowanej wcześniej tabeli
+        dz_dw3 = point[2]  # trzecia losowa wartość z wysolowanej wcześniej tabeli
+        # (są to dokładnie trzy wartości dla jakiegoś miesiąca)
+        dz_db = 1  # ilość współczynnika "bias", czyli b, w naszym wzorze jest tylko jeden
 
         dcost_dz = dcost_dpred * dpred_dz
 
@@ -99,20 +90,6 @@ def train(data):
         b = b - learning_rate * dcost_db
 
     return costs, w1, w2, w3, b
-
-
-def index(request):
-
-    if request.method == "POST":
-        form = SelectProvince_Months_Form(request.POST)
-        if form.is_valid():
-            request.session['provinces'] = request.POST['provinces']
-            request.session['month_from'] = request.POST['month_from']
-            request.session['month_to'] = request.POST['month_to']
-            return redirect('result')
-    else:
-        form = SelectProvince_Months_Form()
-    return render(request, 'App/index.html', {'form': form})
 
 
 def result(request):
@@ -143,8 +120,11 @@ def result(request):
             res = x[key]
             learnValues.append(res)
         arrayLearnValues.append(learnValues)
+        print(arrayLearnValues)
 
     costs, w1, w2, w3, b = train(arrayLearnValues)
+
+    data_for_charts = []
 
     #fig = plt.plot(costs)
     for value in arrayLearnValues:
@@ -156,20 +136,15 @@ def result(request):
               "2016: " + str(value[2]) + "\n" +
               "2017: " + str(value[3]) + "\n" +
               "Predicted value for 2017: " +
-              str(predicted_value)
-              )
-
-#------------------------
-
-    #df = TStat15.objects.all().values()
-    #print(df)
-
-#---------
+              str(predicted_value))
+         data_for_charts.append(value[0], value[1], value[2], value[3], predicted_value)
 
 
 
 
-    return render(request, 'App/index.html')
+
+
+    return render(request, 'App/result.html')
 
     # return render(request, 'App/result.html')
 
